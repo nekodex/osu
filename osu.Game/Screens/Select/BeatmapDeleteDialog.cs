b8fce7db166d859c235425bc94ebb8a9aebf573a
Jamie Taylor <me@nekodex.net>
2021-09-22 19:21:36 +0900

    wip filter fun

--- osu.Game/Screens/Select/BeatmapDeleteDialog.cs
+++ osu.Game/Screens/Select/BeatmapDeleteDialog.cs
@@ -1,7 +1,12 @@
 ﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
+using ManagedBass.Fx;
 using osu.Framework.Allocation;
+using osu.Framework.Audio;
+using osu.Framework.Audio.Mixing;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Sprites;
 using osu.Game.Beatmaps;
 using osu.Game.Overlays.Dialog;
@@ -11,11 +16,31 @@ namespace osu.Game.Screens.Select
     public class BeatmapDeleteDialog : PopupDialog
     {
         private BeatmapManager manager;
+        private AudioMixer mixer;
+        private BQFParameters filter;
+
+        private readonly Bindable<float> filterFreq = new Bindable<float>(22000);
 
         [BackgroundDependencyLoader]
-        private void load(BeatmapManager beatmapManager)
+        private void load(BeatmapManager beatmapManager, AudioManager audio)
         {
             manager = beatmapManager;
+            mixer = audio.TrackMixer;
+
+            filter = new BQFParameters
+            {
+                lFilter = BQFType.LowPass,
+                fCenter = filterFreq.Value,
+                fQ = 1
+            };
+
+            mixer.Effects.Add(filter);
+
+            filterFreq.ValueChanged += e =>
+            {
+                filter.fCenter = e.NewValue;
+                mixer.Effects[0] = filter;
+            };
         }
 
         public BeatmapDeleteDialog(BeatmapSetInfo beatmap)
@@ -36,6 +61,20 @@ namespace osu.Game.Screens.Select
                     Text = @"Firetruck, I didn't mean to!",
                 },
             };
+        }
+
+        protected override void PopIn()
+        {
+            base.PopIn();
+
+            this.TransformBindableTo(filterFreq, 150, 1000);
+        }
+
+        protected override void PopOut()
+        {
+            base.PopOut();
+
+            this.TransformBindableTo(filterFreq, 22000, 1000);
         }
     }
 }
